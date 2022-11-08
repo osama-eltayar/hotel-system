44ddddffb37ab04ec0ed49ce347d293a6dba38f8
osama-eltayar <osamaeltiar@yahoo.com>
2022-11-08 20:45:43 +0200

    project structure

--- app/Models/Hotel.php
+++ app/Models/Hotel.php
@@ -3,40 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 /**
  * @method static self create(array $data)
  */
-class User extends Authenticatable
+class Hotel extends Model
 {
-    use HasFactory, Notifiable;
+    use HasFactory;
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    * The attributes that are mass assignable.
+    * @var array
+    */
     protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
+        'name', 'country_code', 'city_id', 'price'
     ];
 
     //########################################### Constants ################################################
@@ -53,4 +33,6 @@ class User extends Authenticatable
 
     //########################################### Relations ################################################
 
+
 }
+
